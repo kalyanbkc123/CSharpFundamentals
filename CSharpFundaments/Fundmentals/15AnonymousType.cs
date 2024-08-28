53bb7df635c116142b314e90683c031bf6b3a0cb
Kalyan-Charkavarthi <123096261+kalyanbkc123@users.noreply.github.com>
2024-08-28 13:03:50 -0700

    initial commit

---
+++ CSharpFundaments/Fundmentals/15AnonymousType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundaments.Fundmentals
+{
+    public class _15AnonymousType
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous type class..");
+        }
+    }
+}
